1fd6a48876b76c9a0bc675deeb726a90df82b4c8
Chen Yu Pao <windperson@gmail.com>
2018-11-08 13:04:23 +0800

    Add project files.

---
+++ XfBreakout/XfBreakout/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XfBreakout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
